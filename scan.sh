7e60d76d92c5022c02d3712916d13ae299227bd9
d4rkHunt3r <64603382+d4rkHunt3r@users.noreply.github.com>
2020-04-30 12:53:22 -0400

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+read -p "Put the ip address here: " IP
+
+nmap -sV -sC $IP > $IP.nmap
+
+echo "The file is saved in text format in the current directory. Type view to see it or 0 for exit:"
+
+read VIEW
+
+if [[ $VIEW==view ]];
+then
+cat $IP.nmap
+elif [[ $VIEW==0 ]];
+then
+exit
+fi
